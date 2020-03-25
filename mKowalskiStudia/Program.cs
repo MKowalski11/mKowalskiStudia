82a0f3b31cc0be318c6d4772a1bfa66c522dac56
MKowalski11 <gasienica98@gmail.com>
2020-03-25 19:53:20 +0100

    Test dotnet run

--- mKowalskiStudia/Program.cs
+++ mKowalskiStudia/Program.cs
@@ -6,7 +6,11 @@ namespace mKowalskiStudia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i =0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+            Console.WriteLine("Hello World!2");
         }
     }
 }
