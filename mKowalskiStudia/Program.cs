49f23debb041be6f515e33ab2d3b9a231fb0ffdd
MKowalski11 <gasienica98@gmail.com>
2020-03-25 19:03:59 +0100

    Pusty projekt

---
+++ mKowalskiStudia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mKowalskiStudia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
